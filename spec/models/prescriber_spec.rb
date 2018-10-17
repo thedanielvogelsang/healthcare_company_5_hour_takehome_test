8bc03bdde47f7984896956120ca61f4fd92e8c43
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-10-17 14:18:38 -0600

    further TDD for prescriber id validations

--- spec/models/prescriber_spec.rb
+++ spec/models/prescriber_spec.rb
@@ -9,4 +9,24 @@ RSpec.describe Prescriber, type: :model do
     it {should validate_presence_of(:id)}
     it {should validate_presence_of(:name)}
   end
+  context 'intializing with int id' do
+    it 'can be created with any integer as id' do
+      prescriber1 = Prescriber.create(id: 48404, name: "Pr")
+      prescriber2 = Prescriber.new(id: "ABAB", name: "Prescriber2")
+      expect(prescriber1).to be_present
+      expect(prescriber2.save).to be false
+      expect(prescriber2.errors
+                      .messages
+                      .values.include?(["id must be an Integer"])).to be true
+    end
+    it 'prescriber ids must still be unique' do
+      prescriber1 = Prescriber.create(id: 48404, name: "Prescriber1")
+      prescriber2 = Prescriber.new(id: 48404, name: "Prescriber2")
+      expect(prescriber1).to be_present
+      expect(prescriber2.save).to be false
+      expect(prescriber2.errors
+                      .messages
+                      .values.include?(["has already been taken"])).to be true
+    end
+  end
 end
