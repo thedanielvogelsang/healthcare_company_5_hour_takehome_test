afad5a4b9bcc31a9d8981aca21c52a14756b5606
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-10-17 17:37:17 -0600

    fixes disease controller #show

--- app/controllers/api/v1/diseases/disease_controller.rb
+++ app/controllers/api/v1/diseases/disease_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::Diseases::DiseaseController < ApplicationController
-  
+
   def index
     render json: Disease.all, status: 202, each_serializer: DiseaseSerializer
   end
 
   def show
-    if Hospital.exists?(params[:id])
+    if Disease.exists?(params[:id])
       render json: Disease.find(params[:id]), status: 202, serializer: DiseaseSerializer
     else
       render json: {error: "Disease could not be found by that id"}, status: 404
